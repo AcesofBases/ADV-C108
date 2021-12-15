f20ef34055949202faaffd18f46d597db6ad615b
AcesofBases <83755522+AcesofBases@users.noreply.github.com>
2021-12-15 21:06:15 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+
+
+function StartClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier =ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/uoUZtsPKB/model.json",modelReady);
+}
+function modelReady()
+{
+    classifier.classify(gotResults);    
+  
+}
+function gotResults(error,results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results)}
+        random_number_r = Math.floor(Math.random() * 255)+1;
+        random_number_g = Math.floor(Math.random() * 255)+1;
+        random_number_b = Math.floor(Math.random() * 255)+1;
+    
+        
+        document.getElementById("detection").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("hearing").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("hearing").innerHTML="Detected Voice Is Of - "+ results[0].label;
+       
+        img1=document.getElementById("img");
+    
+      
+        if(results[0].label=="Meow"){
+        img1.src="Cat.jpg"
+        
+        
+        }
+        else if(results[0].label=="Cow"){
+            img1.src="Cow.jpg"
+          
+        }
+        else if(results[0].label=="Roar"){
+            img1.src="lion.jpg"
+           
+        }
+        else{
+            img1.src="dog.jpg"
+           
+        }
+        
+    
+}
